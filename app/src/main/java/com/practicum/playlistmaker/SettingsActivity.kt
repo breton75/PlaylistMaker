9c75f359bb68fe1f532c07058e5b5ac3c4bf1067
breton75 <breton75@yandex.ru>
2025-01-14 19:32:28 +0300

    промежуточный

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,14 +1,10 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +12,26 @@ class SettingsActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_settings)
 
-        val btnBack = findViewById<ImageButton>(R.id.btn_arrow_back)
+        val btnBack    = findViewById<ImageButton>(R.id.btn_arrow_back)
+        val btnSupport = findViewById<ImageButton>(R.id.btn_support)
 
         btnBack.setOnClickListener {
             val backIntent = Intent(this, MainActivity::class.java)
             startActivity(backIntent)
+        }
+
+        btnSupport.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.setData(Uri.parse("mailto:"))
+            intent.setType("message/rfc822")
+
+            intent.putExtra(Intent.EXTRA_EMAIL,     "sviridov.sergey.0@yandex.ru")
+            intent.putExtra(Intent.EXTRA_SUBJECT,   "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            intent.putExtra(Intent.EXTRA_TEXT,      "Спасибо разработчикам и разработчицам за крутое приложение!");
+
+            if(intent.resolveActivity(packageManager) != null)
+                startActivity(intent)
         }
     }
 }
